7911c01a30cb8ebc6b3b18d774df4cda9c1cbf2d
uzoma uche <paradise4uzzy@gmail.com>
2019-10-22 07:17:50 +0100

    update app route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes =  [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
